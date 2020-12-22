745965b906c53fb53a7a3af2dde123b8b4211a89
freddie jones <freddie.jones07@hotmail.co.uk>
2020-12-22 20:23:07 +0000

    Cleanup, implement some controllers and UseCases

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,19 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Application.Activities;
 using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
 using Persistence;
 
 namespace API
@@ -47,10 +39,6 @@ namespace API
       });
       services.AddMediatR(typeof(List.Handler).Assembly);
       services.AddControllers();
-      services.AddSwaggerGen(c =>
-      {
-        c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-      });
     }
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -58,8 +46,6 @@ namespace API
       if (env.IsDevelopment())
       {
         app.UseDeveloperExceptionPage();
-        app.UseSwagger();
-        app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
       }
 
       // app.UseHttpsRedirection();
--- Application/Activities/Delete.cs
+++ Application/Activities/Delete.cs
@@ -6,17 +6,11 @@ using Persistence;
 
 namespace Application.Activities
 {
-  public class Edit
+  public class Delete
   {
     public class Command : IRequest
     {
       public Guid Id { get; set; }
-      public string Title { get; set; }
-      public string Description { get; set; }
-      public string Category { get; set; }
-      public DateTime? Date { get; set; }
-      public string City { get; set; }
-      public string Venue { get; set; }
     }
 
     public class Handler : IRequestHandler<Command>
@@ -34,12 +28,7 @@ namespace Application.Activities
         if (activity == null)
           throw new Exception("Could not find activity");
 
-        activity.Title = request.Title ?? activity.Title;
-        activity.Description = request.Description ?? activity.Description;
-        activity.Category = request.Category ?? activity.Category;
-        activity.Date = request.Date ?? activity.Date;
-        activity.City = request.City ?? activity.City;
-        activity.Venue = request.Venue ?? activity.Venue;
+        _context.Remove(activity);
 
         var changedMadeToDb = await _context.SaveChangesAsync();
         var success = changedMadeToDb > 0;
